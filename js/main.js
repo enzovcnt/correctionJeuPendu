09a3459b03eb5e1214ba966b408184394e192221
enzo <enzovcnt@gmail.com>
2024-11-20 00:50:07 +0100

    add body part when error and I can replay the game but just once and the hangman doesn't reset

---
+++ js/main.js
@@ -0,0 +1,341 @@
+const gameKeys = document.querySelector('.gameKeys');
+let alphabet = "abcdefghijklmnopqrstuvwxyz"
+let alphabetArray = alphabet.split('')
+const words = ["bouteille", "casserole", "fourchette", "spatule", "passoire", "cocorico"]
+const displayedWord = document.querySelector(".displayedWord")
+const victoryDiv = document.querySelector(".victory")
+let secretWord
+let displayedSecretWord
+let mistakenLetters
+let maxErrors = 6
+let errors
+console.log("DEBUT : ", displayedSecretWord)
+
+
+console.log(document.querySelectorAll('.keyButton') )
+
+function checkSecretWordForLetter(letterToFind) {
+    if(!secretWord.includes(letterToFind))
+    {
+        return false
+    }
+    let indexes= []
+
+    let secretWordArray = secretWord.split('')
+
+    for(let i = 1; i < secretWordArray.length -1; i++)
+    {
+        if(secretWordArray[i] === letterToFind)
+        {
+            indexes.push(i)
+        }
+    }
+
+
+    return indexes
+
+}
+
+function playLetter(playedLetter) {
+
+    console.log(playedLetter)
+    let indexes = checkSecretWordForLetter(playedLetter)
+
+    if(!indexes){
+        return wrongLetter(playedLetter)
+
+    }
+
+    return addLetterToWord(playedLetter, indexes)
+
+
+}
+
+function addLetterToWord(letterToAdd, indexes) {
+    let displayedSecretWordArray = displayedSecretWord.split('')
+
+    indexes.forEach(index => {
+        displayedSecretWordArray[index] = letterToAdd
+    })
+
+    console.log("MILIEU : ", displayedSecretWord)
+
+    displayedSecretWord = displayedSecretWordArray.toString().replaceAll(',', '')
+    console.log("FIN : ", displayedSecretWord)
+    displayGameWord()
+    disableUsedLettersFromKeyboard()
+    checkForWinOrLost()
+
+}
+
+function createKeyboard(){
+    document.querySelectorAll('.gameKeys>*').forEach(button => {
+        console.log(button)
+        button.remove()
+    })
+
+    alphabetArray.forEach((letter) => {
+        let newKey = document.createElement("button");
+        newKey.classList.add('btn');
+        newKey.classList.add('keyButton');
+        newKey.id = letter;
+        newKey.classList.add('btn-outline-primary');
+        newKey.textContent = letter;
+
+        gameKeys.appendChild(newKey);
+
+
+    })
+    document.querySelectorAll('.keyButton').forEach((gameKey) => {
+        gameKey.addEventListener('click', e => {
+
+            console.log(gameKey.id);
+        })
+    })
+
+    wireKeyButtons()
+
+}
+function initGame() {
+    errors = 0
+    mistakenLetters = []
+    createKeyboard()
+    pickRandomWord()
+    prepareDisplayedSecretWord()
+    displayGameWord()
+    disableUsedLettersFromKeyboard()
+}
+function pickRandomWord(){
+    let randomIndex = Math.floor(Math.random() * words.length);
+    secretWord = words[randomIndex];
+}
+function displayGameWord(){
+    displayedWord.innerHTML = displayedSecretWord
+}
+function prepareDisplayedSecretWord(){
+    displayedSecretWord = ""
+    // premiere et derniere lettre
+    // et les lettres similaires à ces dernieres dans le mot. Ex bouteille
+    //                                                          B___E___E
+    let firstLetter = secretWord[0]
+    console.log(firstLetter)
+    let lastLetter = secretWord[secretWord.length - 1]
+
+    for(let i = 0; i < secretWord.length; i++){
+        displayedSecretWord += "_"
+    }
+
+    let displayedSecretWordArray = displayedSecretWord.split('')
+
+    displayedSecretWordArray[0] = firstLetter
+    displayedSecretWordArray[displayedSecretWord.length - 1] = lastLetter
+
+    let secretWordArray = secretWord.split('')
+
+    for(let i = 0; i < secretWordArray.length; i++){
+
+
+        if(i === 0 && i === secretWord.length){ break }
+
+        switch(secretWord[i]){
+
+            case firstLetter:
+                displayedSecretWordArray[i] = secretWord[i]
+                break;
+            case lastLetter:
+                displayedSecretWordArray[i] = secretWord[i]
+                break;
+
+
+        }
+
+    }
+
+    displayedSecretWord = displayedSecretWordArray.toString().replaceAll(',', '')
+
+
+}
+
+function wireKeyButtons(){
+    const allButtons = document.querySelectorAll('.keyButton')
+    allButtons.forEach(button => {
+        button.addEventListener('click', e => {
+            playLetter(button.id)
+        })
+    })
+}
+
+function disableUsedLettersFromKeyboard() {
+    document.querySelectorAll('.keyButton').forEach(button => {
+        if(displayedSecretWord.includes(button.id) || mistakenLetters.includes(button.id)){
+            button.disabled = true
+        }
+    })
+}
+function wrongLetter(mistakenLetter) {
+    errors++
+
+    mistakenLetters.push(mistakenLetter)
+    disableUsedLettersFromKeyboard()
+    checkForWinOrLost()
+    addBodyPart()
+}
+function checkForWinOrLost() {
+    console.log('avant verif errors')
+    if(errors >= maxErrors){
+        return gameOver()
+    }
+    console.log('apres verif errors')
+
+    if(displayedSecretWord === secretWord ){
+        console.log('victoire')
+
+        return  victory()
+    }
+}
+function gameOver(){
+    let gameOverMessage = document.createElement('span')
+    gameOverMessage.classList.add('.gameOverMessage')
+    gameOverMessage.textContent = "Perdu ! Le mot était :"
+    document.querySelector('.gameDisplay').appendChild(gameOverMessage)
+    displayedWord.textContent = secretWord
+    let playAgainButton = document.createElement('button')
+    playAgainButton.classList.add('btn')
+    playAgainButton.classList.add('btn-primary')
+    playAgainButton.classList.add('rejouero')
+    playAgainButton.innerHTML = "Rejouer"
+    document.querySelector('.gameDisplay').appendChild(playAgainButton)
+
+    document.querySelectorAll('.keyButton').forEach(button => {
+        button.disabled = true
+    })
+
+    document.querySelector('.rejouero').addEventListener('click', e => {
+        console.log('coucou')
+        playAgain()
+        gameOverMessage.style.display = 'none';
+        playAgainButton.style.display = 'none';
+    })
+}
+function victory(){
+    victoryDiv.style.display = "flex"
+    document.querySelector('.rejouer').addEventListener('click', e => {
+        playAgain();
+    })
+}
+function playAgain(){
+    initGame();
+    victoryDiv.style.display = "none";
+}
+function addBodyPart(){
+    switch (errors){
+        case errors = 0:
+            canvasPendu.style.display = "none"
+            break;
+        case errors = 1:
+            head()
+            console.log("1")
+            break;
+        case errors = 2:
+            corps()
+            console.log("2")
+            break;
+        case errors = 3:
+            bras()
+            console.log("3")
+            break;
+        case errors = 4:
+            autreBras()
+            console.log("4")
+            break;
+        case errors = 5:
+            jambeDroite()
+            console.log("5")
+            break;
+        case errors = 6:
+            jambeGauche()
+            console.log("6")
+            break;
+    }
+}
+
+
+initGame()
+
+/// DESSIN DU PENDU
+
+let canvasPendu = document.querySelector('.pendu')
+let dessin = canvasPendu.getContext('2d')
+
+function head(){
+    dessin.beginPath()
+    dessin.strokeStyle = "#000"
+    dessin.lineWidth = 2
+    dessin.arc(70,30, 10, 0, Math.PI * 2, true)
+    dessin.stroke()
+}
+function corps(){
+    dessin.moveTo(70, 40)
+    dessin.lineTo(70,80)
+    dessin.stroke()
+}
+function bras(){
+    dessin.moveTo(70, 50)
+    dessin.lineTo(50,70)
+    dessin.stroke()
+}
+function autreBras(){
+    dessin.moveTo(70, 50)
+    dessin.lineTo(90,70)
+    dessin.stroke()
+}
+function jambeGauche(){
+    dessin.moveTo(70, 80)
+    dessin.lineTo(80,100)
+    dessin.stroke()
+}
+function jambeDroite(){
+    dessin.moveTo(70, 80)
+    dessin.lineTo(60,100)
+    dessin.stroke()
+}
+
+
+
+
+//tete
+//dessin.beginPath()
+//dessin.strokeStyle = "#000"
+//dessin.lineWidth = 2
+//dessin.arc(70,30, 10, 0, Math.PI * 2, true)
+//dessin.stroke()
+
+//corps
+//dessin.moveTo(70, 40)
+//dessin.lineTo(70,80)
+//dessin.stroke()
+
+//bras
+
+//dessin.moveTo(70, 50)
+//dessin.lineTo(50,70)
+//dessin.stroke()
+
+
+//autre bras
+//dessin.moveTo(70, 50)
+//dessin.lineTo(90,70)
+//dessin.stroke()
+
+//jambe gauche
+//dessin.moveTo(70, 80)
+//dessin.lineTo(80,100)
+//dessin.stroke()
+
+// jambe droite
+//dessin.moveTo(70, 80)
+//dessin.lineTo(60,100)
+//dessin.stroke()
+
+
+
